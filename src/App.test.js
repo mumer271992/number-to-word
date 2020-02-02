9136a5e718a1223bd499c3aeaa2cf5dc0e6bb13a
Muhammad Umer <mumer271992@gmail.com>
2020-02-02 15:34:26 +0500

    Test cases added fro number converter form

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,70 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  });
+  it('renders without crash', () => {
+    wrapper = mount(<App />);
+    expect(wrapper.length).toEqual(1);
+  });
+  it('all sections render properly', () => {
+    wrapper = mount(<App />);
+    expect(wrapper.find('[data-test="form"]').length).toEqual(1);
+    expect(wrapper.find('[data-test="result-box"]').length).toEqual(1);
+  });
+  it('should produce word by giving digit input', () => {
+    wrapper = mount(<App />);
+    wrapper.find('[data-test="form"] input').simulate('change', {
+      target: {
+        name: 'digit',
+        value: 42
+      }
+    });
+    wrapper.find('[data-test="form"]').simulate('submit');
+    expect(wrapper.find('[data-test="result-box"]').text()).toEqual(
+      'forty-two'
+    );
+  });
+  it('should display validation error on entering alphabats', () => {
+    wrapper = mount(<App />);
+    wrapper.find('[data-test="form"] input').simulate('change', {
+      target: {
+        name: 'digit',
+        value: 'asd'
+      }
+    });
+    expect(wrapper.find('[data-test="validation-error"]').text()).toEqual(
+      'Only numbers are allowed!'
+    );
+  });
+  it('should hide validation error on entering proper digit', () => {
+    wrapper = mount(<App />);
+    wrapper.find('[data-test="form"] input').simulate('change', {
+      target: {
+        name: 'digit',
+        value: 'asd'
+      }
+    });
+    expect(wrapper.find('[data-test="validation-error"]').text()).toEqual(
+      'Only numbers are allowed!'
+    );
+    wrapper.find('[data-test="form"] input').simulate('change', {
+      target: {
+        name: 'digit',
+        value: '42'
+      }
+    });
+    expect(wrapper.find('[data-test="validation-error"]').length).toEqual(0);
+  });
+  it('should display validation error on submitting empty form', () => {
+    wrapper = mount(<App />);
+    wrapper.find('[data-test="form"]').simulate('submit');
+    expect(wrapper.find('[data-test="validation-error"]').text()).toEqual(
+      'Please enter a number!'
+    );
+  });
 });
